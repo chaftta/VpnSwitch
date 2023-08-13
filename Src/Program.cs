639664faa42a0bd52e78185f06b32ff93799efcc
chaftta@gmail.com <chaftta@gmail.com>
2023-08-13 16:49:30 +0900

    フォームが非表示にできなくなっている問題を修正

--- Src/Program.cs
+++ Src/Program.cs
@@ -3,14 +3,16 @@ using System.Windows.Forms;
 
 namespace VpnSwith {
 	static class Program {
+		private static MainForm Form;
 		/// <summary>アプリケーションのメイン エントリ ポイント</summary>
 		[STAThread]
 		static void Main(string[] Args) {
-			// 引数があれば第一引数をVPN名として起動する
-			var VpnName = Args.Length != 0 ? Args[0] : null;
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new MainForm(VpnName));
+			// 引数があれば第一引数をVPN名として起動する
+			var VpnName = Args.Length != 0 ? Args[0] : null;
+			Form = new MainForm(VpnName);
+			Application.Run();
 		}
 	}
 }
